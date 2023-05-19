2032f118a1b56262ab506b9731c2b4482cef3c54
GenGraphic <gengraphicservices@gmail.com>
2023-05-19 08:36:42 +0200

    add back end

--- api/send_email.php
+++ api/send_email.php
@@ -1,32 +1,43 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-    function sendEmail($name ,$email, $message, $subject) {
-        $to = "gengraphicservices@gmail.com"; // Replace with your email address
-        $subject = "$subject";
-        $body = "Name: $name\n";
-        $body .= "Email: $email\n";
-        $body .= "Message:\n$message\n";
-    
-        if (mail($to, $subject, $body)) {
-            return true;
-        } else {
-            return false;
-        }
+require 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+function sendEmail($name, $email, $message, $subject) {
+    $mail = new PHPMailer(true); // Create a new PHPMailer instance
+
+    try {
+        // SMTP configuration for Hostinger
+        $mail->isSMTP();
+        $mail->Host = 'smtp.hostinger.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'your-email@example.com'; // Replace with your email address
+        $mail->Password = 'your-password'; // Replace with your email account password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Set email content
+        $mail->setFrom('your-email@example.com', 'Your Name'); // Replace with your email and name
+        $mail->addAddress($email, $name); // Add recipient email and name
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        // Send the email
+        $mail->send();
+        echo 'Email sent successfully';
+    } catch (Exception $e) {
+        echo 'Error sending email: ' . $mail->ErrorInfo;
     }
-
-// Usage example:
-$to = "gengraphicservices@gmail.com";
-$subject = "Order Confirmation";
-$message = "<h1>Thank you for your purchase!</h1><p>Your order has been confirmed.</p>";
-$name = "Eduard";
-
-if (sendEmail($name, $to, $subject, $message)) {
-    echo "Email sent successfully.";
-} else {
-    echo "Failed to send email.";
 }
+
+// Usage example
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+
+sendEmail($name, $email, $message, $subject);
+
 ?>
